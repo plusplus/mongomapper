d36aa5dabc878b0dbf25f66ee99d5c74a49822fd
Julian Russell <julian@trug1.local>
2010-10-10 13:07:20 +1100

    1.9.2 compatibility

--- lib/mongo_mapper/plugins.rb
+++ lib/mongo_mapper/plugins.rb
@@ -6,7 +6,14 @@ module MongoMapper
     end
 
     def plugin(mod)
-      extend mod::ClassMethods     if mod.const_defined?(:ClassMethods)
+      
+      # for whatever reason mod.const_defined?(:ClassMethods) fails
+      # in ruby 1.9.2
+      # The following substitute should work in 1.8.7 and 1.9.2
+      # the constant names are converted to strings as they're
+      # strings in 1.8.7 and symbols in 1.9.2!
+      extend mod::ClassMethods     if mod.constants.collect {|s| s.to_s}.include?("ClassMethods")
+      
       include mod::InstanceMethods if mod.const_defined?(:InstanceMethods)
       mod.configure(self)          if mod.respond_to?(:configure)
       plugins << mod
